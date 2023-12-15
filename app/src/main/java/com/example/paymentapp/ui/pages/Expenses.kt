02e4c81e33c57610f39acd77607783b91e51a6f5
LaugeSolvang <lasol21@student.sdu.dk>
2023-12-15 17:40:03 +0100

    Fixed data not changing after adding expense

--- app/src/main/java/com/example/paymentapp/ui/pages/Expenses.kt
+++ app/src/main/java/com/example/paymentapp/ui/pages/Expenses.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -31,14 +32,22 @@ import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Expenses(group: Group, navController: NavHostController) {
+fun Expenses(currentGroup: Group, navController: NavHostController) {
     val viewModel: GroupViewModel = viewModel()
 
+    // Observe changes in the groups property of the ViewModel
+    val groups by viewModel.groups.collectAsState(emptyList())
+    val group = groups.find { it.id == currentGroup.id }
+    Log.d("Group4:",groups.toString())
+
     LaunchedEffect(viewModel.needsRefresh) {
-            delay(50)  // Delay for 30 seconds or your preferred time
+        Log.d("ITWORKS","needs ${viewModel.needsRefresh}")
+            //delay(50)  // Delay for 30 seconds or your preferred time
             viewModel.resetRefreshFlag()  // Reset the flag
             viewModel.loadGroups()  // Refresh groups
     }
+    Log.d("Group4:",groups.toString())
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = { navController.navigate("addExpense") }) {
