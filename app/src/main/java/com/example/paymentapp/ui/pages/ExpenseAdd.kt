f2a86f0b5627707e0c359537472066cd6d6f0de7
pahol21 <pahj19@gmail.com>
2023-12-15 12:33:36 +0100

    Small QoL to ExpenseAdd

--- app/src/main/java/com/example/paymentapp/ui/pages/ExpenseAdd.kt
+++ app/src/main/java/com/example/paymentapp/ui/pages/ExpenseAdd.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.paymentapp.viewmodel.GroupViewModel
@@ -57,7 +59,8 @@ fun ExpenseAdd(navController: NavController, viewModel: GroupViewModel, groupId:
             TextField(
                 value = amount,
                 onValueChange = { amount = it },
-                label = { Text("Amount") }
+                label = { Text("Amount") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Row(verticalAlignment = Alignment.CenterVertically) {
